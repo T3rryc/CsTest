e9c2aacb220cfda37f188f85893c01cfd9344c9e
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2021-03-25 15:56:50 +0100

    Add files via upload
    Initial commit

---
+++ TestCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
